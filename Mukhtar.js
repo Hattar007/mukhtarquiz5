1656441edf59f77ac49d19549a2fe99fa117e64b
Hattar007 <mukhti302@gmail.com>
2020-04-09 23:37:36 +0500

    first commit

---
+++ Mukhtar.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Mukhtar extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from Mukhtar Ali!</h1>
+        <h1> </h1>
+        <a href="https://blog.coinbase.com/how-digital-currency-will-change-the-world-310663fe4332?gi=a1977d02473c">how-digital-currency-will-change-the-world</a>
+      </div>
+    ); }
+}
+
+export default Mukhtar;
